088a58ec010f01a8d3916ed62dbcb18bc297cbfa
herry081288 <herry081288@gmail.com>
2024-11-25 12:58:44 +0000

    tambah Update

--- lib/crudfirebase.dart
+++ lib/crudfirebase.dart
@@ -30,6 +30,89 @@ class _CrudFirebaseState extends State<CrudFirebase> {
     }
   }
 
+  //function update
+  void update(String id, Map<String, dynamic> user) {
+    _nameController.text = user['name'];
+    _emailController.text = user['email'];
+    _ageController.text = user['age'].toString();
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Update Data'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                ),
+                TextField(
+                  controller: _ageController,
+                  decoration: const InputDecoration(labelText: 'Age'),
+                  keyboardType: TextInputType.number,
+                ),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () async {
+                  //deklarasi variabel
+                  final name = _nameController.text.trim();
+                  final email = _emailController.text.trim();
+                  final age = int.tryParse(_ageController.text.trim());
+                  if (name.isNotEmpty && email.isNotEmpty && age != null) {
+                    try {
+                      //update data firestore
+                      await FirebaseFirestore.instance
+                          .collection('user')
+                          .doc(id)
+                          .update({
+                        'name': name,
+                        'email': email,
+                        'age': age,
+                      });
+
+                      //clear text
+                      _nameController.clear();
+                      _emailController.clear();
+                      _ageController.clear();
+                      Navigator.of(context).pop();
+                      //debug application
+                      debugPrint(
+                          'User With Id : $id Updated'); //untuk debugging ada console
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('User Telah Update'),
+                        ),
+                      );
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(e.toString()),
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: const Text('Update Data'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'),
+              )
+            ],
+          );
+        });
+  }
+
   //delete
   Future<void> deleteUser(String id) async {
     await FirebaseFirestore.instance.collection('user').doc(id).delete();
@@ -77,7 +160,8 @@ class _CrudFirebaseState extends State<CrudFirebase> {
                 stream:
                     FirebaseFirestore.instance.collection('user').snapshots(),
                 builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (!snapshot.hasData) return const CircularProgressIndicator();
+                  if (!snapshot.hasData)
+                    return const CircularProgressIndicator();
                   return ListView(
                     children: snapshot.data!.docs.map((doc) {
                       final user = doc.data() as Map<String, dynamic>;
@@ -88,7 +172,11 @@ class _CrudFirebaseState extends State<CrudFirebase> {
                           mainAxisSize: MainAxisSize.min,
                           children: [
                             IconButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                //function update
+                                update(doc.id, user);
+                                print('Klik Edit');
+                              },
                               icon: const Icon(Icons.edit),
                             ),
                             IconButton(
